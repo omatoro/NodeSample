99f0501ba21db9e96bf6b09cd896ac6a9302d473
omatoro <okayama@grincode.com>
2013-04-04 23:49:25 +0900

    3個追加

--- 001/999_param.js
+++ 001/999_param.js
@@ -5,6 +5,8 @@ exports.IP      = "192.168.1.202";
 exports.PORT    = 1337;
 exports.HEADER  = { "Content-Type": "text/html; charset=UTF-8" };
 
-exports.DB_NAME = "sampledb";
 exports.DB_IP   = "192.168.1.202";
 exports.DB_PORT = 27017;
+
+exports.DB_NAME = "sampledb";
+exports.DB_029_NAME = "sample029db";
