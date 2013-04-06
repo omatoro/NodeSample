517c35ba4c7cc80ff6d344c4700cccd139d113f0
omatoro <okayama@grincode.com>
2013-04-06 12:52:10 +0900

    4個追加

---
+++ 001/040_socket_ioでconnect以外の標準イベント.js
@@ -0,0 +1,59 @@
+/*
+ * hello, world
+ * IPなど設定：http://testcording.com/?p=1164
+ */
+/*
+ * モジュール読み込み
+ */
+var http = require("http");
+var fs = require("fs");
+var socketio = require("socket.io");
+var setting = require("/home/virtualserver/デスクトップ/project/NodeSample/001/999_param.js");
+
+/*
+ * サーバの作成
+ */
+var server = http.createServer();
+var io = socketio.listen(server);
+
+
+/*
+ * requestイベント受信時の処理(イベントハンドラ)を作成する
+ */
+server.on("request", function(req, res) {
+	// 外部のHTMLデータを読み込み
+	fs.readFile(
+		"/home/virtualserver/デスクトップ/project/NodeSample/001/040_cliant.html",
+		function (err, data) {
+			if (err) { throw err; }
+
+			// HTTPレスポンスヘッダを作成・送信(200:OK,500:ServerError,404:NotFound)
+			res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
+			res.end(data);
+		}
+	);
+});
+
+/*
+ * イベント待受状態を開始する
+ */
+server.listen(setting.PORT, setting.IP);
+
+/*
+ * 通信時の処理
+ */
+io.sockets.on("connection", function (socket) {
+	console.log("サーバー：コネクションが確立されました。");
+});
+
+io.sockets.on("disconnect", function (socket) {
+	console.log("サーバー：コネクションが切断されました。");
+});
+
+
+
+/*
+ * サーバ起動時に表示するログ(起動したことが分かりやすい)
+ */
+console.log("Server running at http://" + setting.IP + ":" + setting.PORT + "/");
+console.log("サーバを終了する際は[ctrl + c]を押してください");
