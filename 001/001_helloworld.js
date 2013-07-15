458e45d3944b1fa5437cdb6a03319a0c93b4e1a8
omatoro <okayama@grincode.com>
2013-07-15 11:33:59 +0900

    修正テスト #1
    改行コードがおかしくなる…

--- 001/001_helloworld.js
+++ 001/001_helloworld.js
@@ -1,1 +1,10 @@
-/* * hello, world * IPなど設定：http://testcording.com/?p=1164 */var http = require("http");http.createServer(function (req, res) {	res.writeHead(200, {"Content-Type": "text/plain"});	res.end("hello, world!\n");}).listen(1337, localhost);
+/*
+ * hello, world
+ * IPなど設定：http://testcording.com/?p=1164
+ */
+
+var http = require("http");
+http.createServer(function (req, res) {
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("hello, world!\n");
+}).listen(1337, localhost);
