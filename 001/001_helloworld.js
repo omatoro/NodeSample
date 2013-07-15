eb4fd9219954e5f1884351c2df35419a80edc7f4
omatoro <okayama@grincode.com>
2013-07-15 11:30:09 +0900

    修正テスト #1

--- 001/001_helloworld.js
+++ 001/001_helloworld.js
@@ -1,10 +1,1 @@
-/*
- * hello, world
- * IPなど設定：http://testcording.com/?p=1164
- */
-
-var http = require("http");
-http.createServer(function (req, res) {
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("hello, world!\n");
-}).listen(1337, "192.168.1.202");
+/* * hello, world * IPなど設定：http://testcording.com/?p=1164 */var http = require("http");http.createServer(function (req, res) {	res.writeHead(200, {"Content-Type": "text/plain"});	res.end("hello, world!\n");}).listen(1337, localhost);
